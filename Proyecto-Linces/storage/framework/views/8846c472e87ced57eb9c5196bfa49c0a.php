58e6fcf3f8ac98e4ed7f4830bb0024ddc57b1718
DanielaDroguett <166835379+DanielaDroguett@users.noreply.github.com>
2024-08-12 21:47:47 -0400

    Actualizacion main

---
+++ Proyecto-Linces/storage/framework/views/8846c472e87ced57eb9c5196bfa49c0a.php
@@ -0,0 +1,4 @@
+<div>
+    <H1>WENAAAAA</H1>
+</div>
+<?php /**PATH C:\Users\Alumno\Documents\GitHub\T2-70-DSW-I\Proyecto-Linces\resources\views/ProyectosView.blade.php ENDPATH**/ ?>
---
+++ Proyecto-Linces/storage/framework/views/91505b73e8ec8692a1392454573212fb.php
@@ -0,0 +1,4 @@
+<div>
+<H1>GetAllProyectosView</H1>
+</div>
+<?php /**PATH C:\Users\Alumno\Documents\GitHub\T2-70-DSW-I\Proyecto-Linces\resources\views/GetAllProyectosView.blade.php ENDPATH**/ ?>
