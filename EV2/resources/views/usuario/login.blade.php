693c3e4a5e9a28957832b2c3086e2a1b5329b2fd
Patricio Melo <42676797+patriciomelor@users.noreply.github.com>
2024-08-14 12:06:46 -0400

    Revert "Estilos de Login"
    This reverts commit 2b6401f8dc1daca3bf76d76cf1cb13865de1b06e.

--- EV2/resources/views/usuario/login.blade.php
+++ EV2/resources/views/usuario/login.blade.php
@@ -6,83 +6,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
-  <!-- Fonts -->
-  <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    <!--CSS Adminlte-->
-    <link href="{{ asset('css/adminlte.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/adminlte.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
-
-
-    <!--Font icon-->
-    <script src="{{ asset('js/adminlte.min.js') }}"></script>
-    <script src="{{ asset('js/adminlte.js') }}"></script>
-    <!-- Scripts -->
-
 </head>
-<body class="login-page" style="min-height: 496.797px;">
-<div class="login-box">
-  <div class="login-logo">
-    <a href="#"><b>Admin</b>LTE</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
 
-   
-      @if ($errors->any())
+<body>
+    <h1>Inicio de sesión</h1>
+
+    <!-- Errores -->
+    @if ($errors->any())
         @foreach ($errors->all() as $error)
             <li>{{$error}}</li>
         @endforeach
     @endif
 
-    <form class="form-group"action="{{Route('usuario.validar')}}" method="POST">
+    <form action="{{Route('usuario.validar')}}" method="POST">
         @csrf
-  
-        <div class="input-group mb-3">
-        <input class="form-control @error('email') is-invalid @enderror"required autofocus type="email" name="email" placeholder="Ingrese email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-        <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" required  placeholder="Ingrese contraseña">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-               Recuerdame
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-      <a href="{{Route('usuario.registrar')}}">Crear nuevo usuario</a>
-
-    </div>
-    <!-- /.login-card-body -->
-  </div>
-</div>
-<!-- /.login-box -->
-
-<script src="{{ asset('js/jquery/jquery.js') }}"></script>
-<script src="{{ asset('js/jquery/jquery.min.js') }}"></script>
-
+        <input type="text" name="email" placeholder="Ingrese email"><br>
+        <input type="password" name="password" placeholder="Ingrese contraseña"><br>
+        <button type="submit">Ingresar</button>
+    </form>
+    <hr>
+    <a href="{{Route('usuario.registrar')}}">Crear nuevo usuario</a>
 </body>
+
+</html>
--- EV2/vendor/autoload.php
+++ EV2/vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit0587c67fbabd45eaf9b0f1dc04a0e1d4::getLoader();
+return ComposerAutoloaderInit7e8c3c14ff33b199b4a0838993eb8423::getLoader();
